d2d1b66d34c974fdadc1d9c5444be8f7f7b64e17
tianyirenjian <179063828@qq.com>
2021-11-07 19:54:14 +0800

    简化部分代码

--- src/main/java/com/tianyisoft/jvalidate/validators/AcceptedValidator.java
+++ src/main/java/com/tianyisoft/jvalidate/validators/AcceptedValidator.java
@@ -3,18 +3,13 @@ package com.tianyisoft.jvalidate.validators;
 import com.tianyisoft.jvalidate.annotations.Accepted;
 import com.tianyisoft.jvalidate.utils.Tuple2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AcceptedValidator extends Validator {
     public Tuple2<Boolean, String> validate(Accepted accepted, Class<?> klass, Object object, String fieldName) throws NoSuchFieldException, IllegalAccessException {
         Object o = getFieldValue(klass, object, fieldName);
-        List<String> acceptedValues = new ArrayList<String>() {{
-            add("on");
-            add("yes");
-            add("1");
-            add("true");
-        }};
+        List<String> acceptedValues = Arrays.asList("on", "yes", "1", "true");
         if (o == null || (o instanceof String && (acceptedValues.contains(o)))) {
             return trueResult();
         }
