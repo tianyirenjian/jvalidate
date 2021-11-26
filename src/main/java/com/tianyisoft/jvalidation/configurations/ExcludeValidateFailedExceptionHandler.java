f20923601354ab900b563fa83ce3781865b97553
yinyanjun <179063828@qq.com>
2021-11-26 18:11:15 +0800

    修改错误格式，支持自定义错误返回格式,优化部分代码

---
+++ src/main/java/com/tianyisoft/jvalidation/configurations/ExcludeValidateFailedExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.tianyisoft.jvalidation.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExcludeValidateFailedExceptionHandler {
+    @Bean
+    public void validateFailedExceptionHandler() {}
+}
