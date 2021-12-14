0a3d4e4923cc62bb2b2d682ed42941e5bb768ef8
yinyanjun <179063828@qq.com>
2021-12-14 14:37:51 +0800

    添加 BindingErrors 类用来接收错误

--- src/main/java/com/tianyisoft/jvalidation/controllers/UserController.java
+++ src/main/java/com/tianyisoft/jvalidation/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.tianyisoft.jvalidation.controllers;
 
 import com.tianyisoft.jvalidate.annotations.JValidated;
+import com.tianyisoft.jvalidate.utils.BindingErrors;
 import com.tianyisoft.jvalidation.pojos.Create;
 import com.tianyisoft.jvalidation.pojos.Update;
 import com.tianyisoft.jvalidation.pojos.User;
@@ -14,7 +15,10 @@ public class UserController {
 
     @JValidated
     @PostMapping("/users")
-    public User store(@RequestBody @JValidated(groups = {Create.class, Update.class}) User user) {
-        return user;
+    public ResponseEntity<?> store(@RequestBody @JValidated(groups = {Create.class, Update.class}) User user, BindingErrors bindingErrors) {
+        if (bindingErrors.hasErrors()) {
+            return ResponseEntity.status(400).body(bindingErrors.getErrors());
+        }
+        return ResponseEntity.ok(user);
     }
 }
